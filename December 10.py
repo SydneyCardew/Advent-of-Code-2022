69f943386dfc3d35339a2a3113955297a7f6228d
SydneyCardew <64916220+SydneyCardew@users.noreply.github.com>
2022-12-10 21:01:18 +0000

    Update December 10.py

--- December 10.py
+++ December 10.py
@@ -1,4 +1,8 @@
 targets = [x for x in range(221) if (x + 20) % 40 == 0]
+cycle = 1
+register = 1
+answers = []
+screen = [['.' for x in range(40)] for y in range(6)]
 
 
 def check(cycles, answers, register):
@@ -9,6 +13,7 @@ def check(cycles, answers, register):
 
 def draw(register, cycle, screen):
     sprite_pos = [register - 1, register, register + 1]
+    print (register, sprite_pos)
     row = cycle // 40
     column = (cycle % 40) - 1
     if column in sprite_pos:
@@ -24,25 +29,16 @@ def cycler(cycle, register, answers, screen):
 
 
 def parser(instruction, cycle, register, answers, screen):
-    if instruction == 'noop':
-        cycle, register, answers, screen = cycler(cycle, register, answers, screen)
-    else:
-        cycle, register, answers, screen = cycler(cycle, register, answers, screen)
+    cycle, register, answers, screen = cycler(cycle, register, answers, screen)
+    if 'addx' in instruction:
         register += int(instruction.split()[1])
         cycle, register, answers, screen = cycler(cycle, register, answers, screen)
     return cycle, register, answers, screen
 
 
 with open("input_10.txt", "r+")as input_file:
-    cycle = 1
-    register = 1
-    answers = []
-    screen = [['.' for x in range(40)] for y in range(6)]
     for line in input_file:
         cycle, register, answers, screen = parser(line.strip("\n"), cycle, register, answers, screen)
     print(sum(answers))
     for line in screen:
         print("".join(line))
-
-
-
